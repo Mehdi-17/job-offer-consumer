7282c186c18fe174e84c186de3fea76e565efa74
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-22 08:07:57 +0200

    Initial Commit

---
+++ src/main/java/com/jobmarketanalyzer/job_offer_consumer/JobOfferConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.jobmarketanalyzer.job_offer_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferConsumerApplication.class, args);
+	}
+
+}
