2ec9f999953f93a0112cb4af48dc5ef2a562d9a1
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-23 08:24:03 +0200

    Create JobOffer model to save job offer in database

---
+++ src/main/java/com/jobmarketanalyzer/job_offer_consumer/model/JobOffer.java
@@ -0,0 +1,30 @@
+package com.jobmarketanalyzer.job_offer_consumer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JobOffer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String source;
+    private String title;
+    private String description;
+    private String location;
+    private String company;
+}
