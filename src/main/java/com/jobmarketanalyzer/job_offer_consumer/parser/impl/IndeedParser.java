9a7daa7cae258590abcce0e2f6a939e1bb97d551
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-28 08:09:38 +0200

    Extract salaries from Indeeds string

--- src/main/java/com/jobmarketanalyzer/job_offer_consumer/parser/impl/IndeedParser.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_consumer/parser/impl/IndeedParser.java
@@ -15,8 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @RequiredArgsConstructor
@@ -35,16 +39,17 @@ public class IndeedParser implements JobOfferParser {
             RefOfferSource source = refOfferSourceRepository.findBySource(SourceOffer.INDEED)
                     .orElseThrow(() -> new NoSuchElementException("Source " + SourceOffer.INDEED + " not found in the database."));
 
-            return jobOfferDTOS.stream().map(jobOfferDTO ->
-                            JobOffer.builder()
-                                    .source(source)
-                                    .title(jobOfferDTO.title())
-                                    .date(LocalDate.now())
-                                    //todo to parse through jobOfferDTO.dailyRate()
-                                    .minSalary(null)
-                                    .maxSalary(null)
-                                    .description(cleanText(jobOfferDTO.description()))
-                                    .build())
+            return jobOfferDTOS.stream().map(jobOfferDTO -> {
+                        SalaryRange minAndMaxSalaries = getMinAndMaxSalariesFromString(jobOfferDTO.dailyRate());
+                        return JobOffer.builder()
+                                .source(source)
+                                .title(jobOfferDTO.title())
+                                .date(LocalDate.now())
+                                .minSalary(minAndMaxSalaries.minSalary())
+                                .maxSalary(minAndMaxSalaries.maxSalary())
+                                .description(cleanText(jobOfferDTO.description()))
+                                .build();
+                    })
                     .toList();
         } catch (JsonProcessingException e) {
             log.error("Unable to parse Job offers from json.", e);
@@ -60,4 +65,34 @@ public class IndeedParser implements JobOfferParser {
         String withoutHtml = textWithHtml.replaceAll("<[^>]*>", "");
         return withoutHtml.replaceAll("[\n\r\t]+", " ").trim();
     }
+
+    private SalaryRange getMinAndMaxSalariesFromString(String salaries) {
+        List<Long> salariesList = extractNumbersFromString(salaries);
+        Long minSalary = salariesList.isEmpty() ? 0 : salariesList.get(0);
+        Long maxSalary = salariesList.size() > 1 ? salariesList.get(1) : 0;
+        return new SalaryRange(minSalary, maxSalary);
+    }
+
+    private List<Long> extractNumbersFromString(String input){
+        List<Long> numbers = new ArrayList<>();
+
+        //Regex pour garder les nombre en ignorant les espaces et les séparateurs de milliers
+        Pattern pattern = Pattern.compile("\\b\\d+(?:[\\s,.]\\d+)*\\b");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            // Supprimer les espaces et les séparateurs de milliers
+            String numberStr = matcher.group().replaceAll("[\\s,.]", "");
+            try {
+                numbers.add(Long.parseLong(numberStr));
+            } catch (NumberFormatException e) {
+                log.error("The string does not have the appropriate format to be parse to long : ", e);
+            }
+        }
+
+        Collections.sort(numbers);
+        return numbers;
+    }
+
+    private record SalaryRange(Long minSalary, Long maxSalary){}
 }
