545a5cf8ee8656f2086b1146f11c48f7f6032399
Mehdi-17 <mehdi.serra57@gmail.com>
2024-09-24 08:16:26 +0200

    Add company to field JobOfferDTO

--- src/main/java/com/jobmarketanalyzer/job_offer_consumer/DTO/JobOfferDTO.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_consumer/DTO/JobOfferDTO.java
@@ -1,4 +1,4 @@
 package com.jobmarketanalyzer.job_offer_consumer.DTO;
 
-public record JobOfferDTO(String title, String description, String dailyRate) {
+public record JobOfferDTO(String title, String description, String dailyRate, String company) {
 }
